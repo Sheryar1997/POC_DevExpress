79889c8e3cffef2a9f7d07f89871387664c3de50
AyihanMarkcoders2023 <ayihanulhaq.markcoders@gmail.com>
2023-12-27 18:59:26 +0500

    Add CORS headers to allow cross-origin requests

--- server/index.js
+++ server/index.js
@@ -9,6 +9,12 @@ const { connect } = require("./config/Database");
 connect();
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
