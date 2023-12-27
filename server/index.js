d26ea1097a9a4a24b3241f22f9d77db38c7e9c7c
AyihanMarkcoders2023 <ayihanulhaq.markcoders@gmail.com>
2023-12-27 16:45:03 +0500

    Add CORS middleware to server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 4000;
 const pieChartRoutes = require("./routes/pieChart");
 const lineChartRoutes = require("./routes/lineChart");
 
 const { connect } = require("./config/Database");
 connect();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
