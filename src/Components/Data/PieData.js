e9cff9883e8ccdddc606f536cfdb0971e81f34ba
AyihanMarkcoders2023 <ayihanulhaq.markcoders@gmail.com>
2023-12-27 14:53:54 +0500

    Add details to PieData.js

--- src/Components/Data/PieData.js
+++ src/Components/Data/PieData.js
@@ -1,25 +1,117 @@
 export const areas = [{
     country: 'Russia',
     area: 12,
+    details: [{
+      name: 'Moscow',
+      count: 12
+    }, {
+      name: 'St.Petersburg',
+      count: 5
+    }, {
+      name: 'Kazan',
+      count: 3
+    }, {
+      name: 'Novosibirsk',
+      count: 1
+    }, {
+      name: 'Others',
+      count: 2
+    }]
   }, {
     country: 'Canada',
     area: 7,
+    details: [{
+      name: 'Toronto',
+      count: 3
+    }, {
+      name: 'Ottawa',
+      count: 1
+    }, {
+      name: 'Montreal',
+      count: 2
+    }, {
+      name: 'Vancouver',
+      count: 1
+    }]
   }, {
     country: 'USA',
     area: 7,
+    details: [{
+      name: 'New York',
+      count: 5
+    }, {
+      name: 'Los Angeles',
+      count: 1
+    }, {
+      name: 'Chicago',
+      count: 1
+    }]
   }, {
     country: 'China',
     area: 7,
+    details: [{
+      name: 'Beijing',
+      count: 3
+    }, {
+      name: 'Shanghai',
+      count: 2
+    }, {
+      name: 'Hong Kong',
+      count: 1
+    }, {
+      name: 'Guangzhou',
+      count: 1
+    }]
   }, {
     country: 'Brazil',
     area: 6,
+    details: [{
+      name: 'Sao Paulo',
+      count: 2
+    }, {
+      name: 'Rio de Janeiro',
+      count: 2
+    }, {
+      name: 'Brasilia',
+      count: 1
+    }, {
+      name: 'Fortaleza',
+      count: 1
+    }]
   }, {
     country: 'Australia',
     area: 5,
+    details: [{
+      name: 'Sydney',
+      count: 3
+    }, {
+      name: 'Melbourne',
+      count: 2
+    }]
   }, {
     country: 'India',
     area: 2,
+    details: [{
+      name: 'Mumbai',
+      count: 1
+    }, {
+      name: 'New Delhi',
+      count: 1
+    }]
   }, {
     country: 'Others',
     area: 55,
+    details: [{
+      name: 'Uzbekistan',
+      count: 12
+    }, {
+      name: 'Ukraine',
+      count: 5
+    }, {
+      name: 'Turkmenistan',
+      count: 3
+    }, {
+      name: 'Tajikistan',
+      count: 1
+    }]
   }];
