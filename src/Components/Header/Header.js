95ef6fb32236457dd1f9516278e395a41f3dc8ad
Sheryar1997 <shehryar.markcoders@gmail.com>
2023-12-28 15:55:13 +0500

    dropdown added in sidebar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,115 +1,122 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useEffect, useRef, useState } from "react";
-import { Col, Row, NavDropdown, Navbar, Nav } from "react-bootstrap";
+import { Col, Row, Navbar, Nav, NavDropdown } from "react-bootstrap";
 import "./Header.css";
 
-function Header({ sideBarItems, fullScreen, closeScreen, handle, children }) {
+function Header({ sideBarItems, children }) {
   const { pathname } = useLocation();
   const navigate = useNavigate();
-  const navbarRef = useRef();
-  const rightNavRef = useRef();
-  const leftNavRef = useRef();
-
-  const [sidebarToggle, setSidebarToggle] = useState(false)
+  const [sidebarToggle, setSidebarToggle] = useState(false);
+  const [chartsSubmenuOpen, setChartsSubmenuOpen] = useState(false);
 
   const classes = (path) => {
     let splitPath = path.split("/");
     let splitPathname = pathname.split("/");
-
-    if (splitPath[1] === splitPathname[1]) {
-      return "nav_active";
-    }
-
-    return "";
+    return splitPath[1] === splitPathname[1] ? "nav_active" : "";
   };
 
-  const NavHandler = () => {
-    
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  const handleSubmenuItemClick = (e) => {
+    e.stopPropagation(); // Prevents the event from propagating to the parent "Charts" item
   };
 
   return (
-    <>
-      <div className="user_layout">
-        <div className="layout_content_section">
-
-          <div ref={leftNavRef} className={sidebarToggle ? "left_layout_overlay shrink" : "left_layout_overlay"}>
-
-            <div className={sidebarToggle ? "layout_content_sidebar_section close" : "layout_content_sidebar_section"}>
-              <div className="user_sidebar">
-                <div className="sidebar_top_logo" style={sidebarToggle ? { justifyContent: "center" } : null}>
-                  <img
-                    src="/images/toggle_icon.png"
-                    alt=""
-                    className="toggle_icon"
-                    onClick={() => setSidebarToggle(!sidebarToggle)}
-                  />
-                </div>
-
-                <ul className="nav_list">
-                  <h6 className={sidebarToggle ? 'toggler' : null}>{sidebarToggle ? 'Menu' : "Main Menu"}</h6>
-                  {sideBarItems?.map((item, index) => {
-                    if (item.path) {
-                      return (
-                        <li key={index} className={`${classes(item.path)}`}>
-                          <Link to={item.path} style={sidebarToggle ? { padding: '12px 0px 12px 15px' } : null}>
-                            <img src={item.icon} alt="" />
-                            <span style={sidebarToggle ? { display: "none" } : null}>{item.title}</span>
-                          </Link>
-                        </li>
-                      );
-                    }
-                  })}
-                </ul>
+    <div className="user_layout">
+      <div className="layout_content_section">
+        <div ref={useRef()} className={sidebarToggle ? "left_layout_overlay shrink" : "left_layout_overlay"}>
+          <div className={sidebarToggle ? "layout_content_sidebar_section close" : "layout_content_sidebar_section"}>
+            <div className="user_sidebar">
+              <div className="sidebar_top_logo" style={sidebarToggle ? { justifyContent: "center" } : null}>
+                <img
+                  src="/images/toggle_icon.png"
+                  alt=""
+                  className="toggle_icon"
+                  onClick={() => setSidebarToggle(!sidebarToggle)}
+                />
               </div>
-            </div>
-          </div>
-
-          <div className={sidebarToggle ? "layout_content shrink" : "layout_content"}>
-            <div className="user_header">
-
-              <Row className="align-items-center make_col_reverse">
-                <Col md={6}>
-
-                </Col>
-                <Col md={6}>
-                  <Navbar collapseOnSelect expand="lg">
-                    <Navbar.Brand onClick={() => navigate("/")}>
-                      <img src="/images/ajcl_logo.png" alt="" className="mob_responsive_logo" />
-                    </Navbar.Brand>
-                    <Navbar.Toggle
-                      aria-controls="responsive-navbar-nav"
-                      onClick={NavHandler}
-                    />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                      <Nav className="ms-auto">
-                        <div className="nav_header_right">
-                          <div className="user_nav">
-                            {/* <img src="/images/user_img.png" alt="" /> */}
-
-                            <NavDropdown title="Admin" id="basic-nav-dropdown">
-                              <NavDropdown.Item>
-                                <Link to='/profile'>Profile</Link>
-                              </NavDropdown.Item>
-                              <NavDropdown.Item href="/">
-                                <Link>Logout</Link>
-                              </NavDropdown.Item>
-                            </NavDropdown>
-                          </div>
-                        </div>
-                      </Nav>
-                    </Navbar.Collapse>
-                  </Navbar>
-                </Col>
-              </Row>
-            </div>
-
-            <div className="right_layout_overlay" ref={rightNavRef}>
-              {children}
+              <ul className="nav_list">
+                <h6 className={sidebarToggle ? 'toggler' : null}>{sidebarToggle ? 'Menu' : "Main Menu"}</h6>
+                {sideBarItems.map((item, index) => {
+                  if (item.title === "Charts") {
+                    return (
+                      <li key={index} className={`${classes(item.path)} dropdown-item`} onClick={() => setChartsSubmenuOpen(!chartsSubmenuOpen)}>
+                        <Link to={item.path}>
+                          <img src={item.icon} alt="" />
+                          <span>{item.title}</span>
+                        </Link>
+                        {chartsSubmenuOpen && item.items && item.items.map((subItem, subIndex) => (
+                          <ul key={subIndex} className="submenu">
+                      <li key={subIndex} className={`${classes(subItem.path)}`} onClick={handleSubmenuItemClick}>
+                              <Link to={subItem.path}>
+                                <img src={subItem.icon} alt="" />
+                                <span>{subItem.title}</span>
+                              </Link>
+                            </li>
+                          </ul>
+                        ))}
+                      </li>
+                    );
+                  } else {
+                    return (
+                      <li key={index} className={`${classes(item.path)}`}>
+                        <Link to={item.path}>
+                          <img src={item.icon} alt="" />
+                          <span>{item.title}</span>
+                        </Link>
+                      </li>
+                    );
+                  }
+                })}
+              </ul>
             </div>
           </div>
         </div>
+
+        <div className={sidebarToggle ? "layout_content shrink" : "layout_content"}>
+          <div className="user_header">
+            <Row className="align-items-center make_col_reverse">
+              <Col md={6}>
+                {/* Additional content can be added here if necessary */}
+              </Col>
+              <Col md={6}>
+                <Navbar collapseOnSelect expand="lg">
+                  <Navbar.Brand onClick={() => navigate("/")}>
+                    <img src="/images/ajcl_logo.png" alt="" className="mob_responsive_logo" />
+                  </Navbar.Brand>
+                  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                  <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto">
+                      <div className="nav_header_right">
+                        <div className="user_nav">
+                          {/* <img src="/images/user_img.png" alt="" /> */}
+
+                          <NavDropdown title="Admin" id="basic-nav-dropdown">
+                            <NavDropdown.Item>
+                              <Link to='/profile'>Profile</Link>
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="/">
+                              <Link>Logout</Link>
+                            </NavDropdown.Item>
+                          </NavDropdown>
+                        </div>
+                      </div>
+                    </Nav>
+                  </Navbar.Collapse>
+                </Navbar>
+              </Col>
+            </Row>
+          </div>
+
+          <div className="right_layout_overlay">
+            {children}
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
+
 export default Header;
--- src/Container/Admin/Routes/Routes.js
+++ src/Container/Admin/Routes/Routes.js
@@ -3,7 +3,6 @@ import DevExtreme from "../Pages/DevExtreme/DevExtreme";
 import DevExtremeLine from "../Pages/DevExtremeLine/DevExtremeLine";
 import DevExtremeDataGrid from "../Pages/DevExtremeDataGrid/DevExtremeDataGrid";
 import DynamicPie from "../Pages/DynamicPie/DynamicPie";
-import DevExtremePie from "../Pages/DevExtremePie/DevExtremePie";
 import DevExtremeStack from "../Pages/DevExtremeStackedBar/DevExtremeStack";
 // import DevExtremeDataGrid from "../Pages/DevExtremeDataGrid/DevExtremeDataGrid";
 import DevExtremeScatterChart from "../Pages/DevExtremeScatterChart/DevExtemeScatter";
@@ -47,33 +46,40 @@ export const adminSideBarItems = [
     title: "Dashboard - chart-js",
   },
   {
-    path: "/bar",
-    icon: "/images/consignee_icon.png",
-    title: "Dev Extreme - Bar chart",
-  },
-  {
-    path: "/line",
-    icon: "/images/dc_icon.png",
-    title: "Dev Extreme - Line chart",
-  },
-  {
-    path: "/pie",
-    icon: "/images/enquiry_icon.png",
-    title: "Dev Extreme - Pie chart",
+    path: "#",
+    title: "Charts",
+    icon: "/images/cells.png",
+    items: [ // Nested items for Charts dropdown
+      {
+        path: "/bar",
+        icon: "/images/consignee_icon.png",
+        title: "Dev Extreme - Bar chart",
+      },
+      {
+        path: "/line",
+        icon: "/images/dc_icon.png",
+        title: "Dev Extreme - Line chart",
+      },
+      {
+        path: "/pie",
+        icon: "/images/enquiry_icon.png",
+        title: "Dev Extreme - Pie chart",
+      },
+      {
+        path: "/stack",
+        icon: "/images/dc_icon.png",
+        title: "Dev Extreme - Stacked Bar chart",
+      },
+      {
+        path: "/scatter",
+        icon: "/images/dc_icon.png",
+        title: "Dev Extreme - Scatter chart",
+      }
+    ]
   },
   {
     path: "/data-grid",
     icon: "/images/cells.png",
     title: "Dev Extreme - Data Grid",
-  },
-  {
-    path: "/stack",
-    icon: "/images/dc_icon.png",
-    title: "Dev Extreme - Stacked Bar chart",
-  },
-  {
-    path: "/scatter",
-    icon: "/images/dc_icon.png",
-    title: "Dev Extreme - Scatter chart",
   }
 ];
